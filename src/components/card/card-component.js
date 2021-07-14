bfcb43a69df9f73536f7b81405de0aefc5ee6ab4
saighY <85502813+saighY@users.noreply.github.com>
2021-07-14 15:54:17 -0500

    fix name font in card tile

--- src/components/card/card-component.js
+++ src/components/card/card-component.js
@@ -5,7 +5,7 @@ export const Card = (props) => {
     return (
         <div className="card-container">
             <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2`} />
-            <h1>{props.monster.name}</h1>
+            <h2>{props.monster.name}</h2>
             <p>{props.monster.email}</p>
         </div>
     );
